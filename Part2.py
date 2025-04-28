56734905d3842c1b282fa260310b6593d7516921
Alexander <alex@schwanda.cc>
2025-04-27 21:24:00 -0500

    Innit

--- Part2.py
+++ Part2.py
@@ -1,3 +1,10 @@
 import datetime
 class Inventory:
     def __init__(self):
+        # Data sets, they store things. Specifically the things that the variables are named after.
+        self.manufacturer_data = {}   
+        self.price_data = {}          
+        self.service_date_data = {}   
+        self.inventory = []           
+        self.manufacturer_set = set() # For quick lookup of manufacturer names.
+        self.item_type_set = set()    # same
